59fe82b5c88c53955fe0af1f34e66857b5fe50ec
viogus <zhixun.lin@gmail.com>
2023-01-10 14:44:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,3 +12,6 @@
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+#./scripts/feeds update -a
+#./scripts/feeds install -a
